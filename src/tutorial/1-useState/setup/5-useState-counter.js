ecc3dcee391d424c4e880d5564e02764b7140809
Ajay Bava <ajaybava1998@gmail.com>
2021-03-10 19:48:26 +0000

    commit

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -4,14 +4,32 @@ const UseStateCounter = () => {
 
   const [value, setValue] = useState(0);
 
+  const reset = () => {
+    setValue(0)
+  };
+
+  const complex = () => {
+    setTimeout(() => {
+      setValue((pre) => {
+        return pre + 1;
+      })
+    }, 2000)
+  }
+
   return (
     <>
       <section style={{ margin: '4rem 0' }}>
         <h2>regular counter</h2>
         <h1>{value}</h1>
-        <button className='btn'>decrease</button>
-        <button className='btn'>reset</button>
-        <button className='btn'>increase</button>
+        <button className='btn' onClick={() => setValue(value-1)}>decrease</button>
+        <button className='btn' onClick={reset}>reset</button>
+        <button className='btn' onClick={() => setValue(value+1)}>increase</button>
+      </section>
+
+      <section style={{ margin: '4rem 0' }}>
+        <h2>conmplex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complex}>complex</button>
       </section>
     </>
   );
